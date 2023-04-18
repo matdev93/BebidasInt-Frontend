d418150db6800e1a70fa8a6539ed1c815fd375eb
Cedric Noriega <70352411+CNoriegaTL@users.noreply.github.com>
2023-04-18 18:34:48 -0300

    Se agregar user reducer, provider, context y sign in

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import UserProvider from './context/user/UserProvider';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <UserProvider>
+        <App />
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
